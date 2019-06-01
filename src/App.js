035d58b5f62ca778dfab670f3ec9d5a48141dd15
Stan Kovrigin <stan@argaatas.com>
2019-06-02 01:39:55 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+import UserForm from './components/UserForm';
 
-      </header>
-    </div>
-  );
+// https://api.github.com/users/john
+
+class App extends React.Component {
+     getUser = (e) => {
+        e.preventDefault();
+        const user = e.target.username.value;
+        axios.get(`https://api.github.com/users/${user}`)
+            .then((res) => {
+                console.log(res);
+            });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>HTTP calls</h1>
+                </header>
+                <UserForm getUser={this.getUser}/>
+            </div>
+        );
+    }
 }
 
 export default App;
