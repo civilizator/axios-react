43043788f2367890ab6f143056151cbd5319d1bc
civilizator <stan@argaatas.com>
2019-06-05 01:03:30 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from "axios";
+import axios from 'axios';
 import './App.css';
 
 import UserForm from './components/UserForm';
@@ -7,14 +7,23 @@ import UserForm from './components/UserForm';
 // https://api.github.com/users/john
 
 class App extends React.Component {
-    getUser = (e) => {
+    state = {
+        repos: null
+    };
+
+    getUser = e => {
         e.preventDefault();
         const user = e.target.username.value;
-        axios.get(`https://api.github.com/users/${user}`)
-            .then((res) => {
-                console.log(res);
-            });
-
+        if (user) {
+            axios.get("https://api.github.com/users/" + user)
+                .then(res => {
+                    const repos = res.data.public_repos;
+                    this.setState({repos});
+                    console.log(res);
+                });
+        } else {
+            this.setState({repos: null});
+        }
     };
 
     render() {
@@ -24,9 +33,13 @@ class App extends React.Component {
                     <h1>HTTP calls</h1>
                 </header>
                 <UserForm getUser={this.getUser}/>
+                {this.state.repos
+                    ? <p>Number repose: {this.state.repos}</p>
+                    : <p>Please enter username</p>}
             </div>
         );
     }
 }
+
 
 export default App;
